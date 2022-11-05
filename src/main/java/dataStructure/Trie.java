a051ab07bc849e10dd8c6e33cedff9f00bd7f69f
vitran96 <tranuytrieuvi@gmail.com>
2022-11-05 22:59:26 +0700

    trie

--- src/main/java/dataStructure/Trie.java
+++ src/main/java/dataStructure/Trie.java
@@ -1,4 +1,18 @@
 package dataStructure;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Trie {
+    public static class TrieNode {
+        private final Map<Character, TrieNode> map = new HashMap<>();
+
+        public boolean isEmpty() {
+            return map.isEmpty();
+        }
+
+        public void insert(String[] strings) {
+            //
+        }
+    }
 }
